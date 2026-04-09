6704dca1030ebcb59bf6e144a5c69735c0f08824
Ramya S <ramyascoorg@gmail.com>
2026-04-09 18:24:41 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -409,4 +409,15 @@ export default function LoginPage() {
                     <div className="lp-input-wrap">
                       <svg className="lp-input-icon" viewBox="0 0 24 24">
                         <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                        </div>
+                  </div>
+                </form>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+            }
                 
